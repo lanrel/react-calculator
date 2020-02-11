ea9aae01e4607ed578f83ea557577a25b8bf95d0
unknown <lanrel@yahoo.com>
2020-02-11 09:55:10 +0000

    Updates

---
+++ precache-manifest.f10cf0fb052c5a5532c82aeb0761e5bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018400f165247e4539e4c89127ec6d18",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "2163668625cd5e96d2c9",
+    "url": "/react-calculator/static/css/main.14f2c3b4.chunk.css"
+  },
+  {
+    "revision": "ddd85d0d0fbc1c2d704f",
+    "url": "/react-calculator/static/js/2.eeb15fc6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-calculator/static/js/2.eeb15fc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2163668625cd5e96d2c9",
+    "url": "/react-calculator/static/js/main.2006ae56.chunk.js"
+  },
+  {
+    "revision": "36013ae44c0d5168c12c",
+    "url": "/react-calculator/static/js/runtime-main.c0112216.js"
+  }
+]);
